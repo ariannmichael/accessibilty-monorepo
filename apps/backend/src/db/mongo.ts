8ab65a3e43b1e3243b04a948f8dad5a62a02ff55
ariann-michael <ariann.michael-c@drchrono.com>
2025-11-28 17:42:57 -0300

    MongoDB connection

---
+++ apps/backend/src/db/mongo.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export const connectMongo = async () => {
+    const mongoUri = process.env.MONGODB_URI as string ?? "mongodb://localhost:27017/accessibility-monorepo";
+
+    try {
+        await mongoose.connect(mongoUri);
+        console.log("Connected to MongoDB", mongoUri);
+    } catch (error) {
+        console.error("Error connecting to MongoDB", error);
+        process.exit(1);
+    }
+};
